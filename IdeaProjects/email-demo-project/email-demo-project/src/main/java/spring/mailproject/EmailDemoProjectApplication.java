0e5206f628fb12948349252252ff8d49444aef83
shannon <shannonchimutengo@gmail.com>
2022-08-18 21:59:23 +0200

    added other file

---
+++ IdeaProjects/email-demo-project/email-demo-project/src/main/java/spring/mailproject/EmailDemoProjectApplication.java
@@ -0,0 +1,14 @@
+package spring.mailproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailDemoProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EmailDemoProjectApplication.class, args);
+	}
+
+}
